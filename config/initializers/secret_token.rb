6378c6b2201b4c346800004e48b11b736758a2c1
simlegate <simlegate@163.com>
2013-01-06 05:12:56 +0800

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Attendance::Application.config.secret_token = '3c74435a913f46bbcd673ba664f9310071de4b745128e8e67901742b677f2a1dcd438cf8dad966c476a27c8812c71bd9bc291a60817436131622e9101640c03f'
