06896b0a6fc0ec11034f57d3d742f43b76f39892
Byronlee <lbyronlee@gmail.com>
2013-01-08 03:54:09 +0800

    byronlee

--- app/controllers/person_controller.rb
+++ app/controllers/person_controller.rb
@@ -4,4 +4,16 @@ class PersonController < ApplicationController
 
   def clerk
   end
+
+  def mark_clerk
+  end
+  
+  def  minister_view
+  end
+
+  def  minister_view_1
+  end
+
+  def leader_view
+  end
 end
