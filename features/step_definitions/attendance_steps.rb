50358139af45cff061a1e51d141f64ac252c6ba2
simlegate <simlegate@163.com>
2013-01-23 23:05:56 +0800

    添加一组例子数据

--- features/step_definitions/attendance_steps.rb
+++ features/step_definitions/attendance_steps.rb
@@ -1,23 +1,12 @@
 # encoding: utf-8
-
-
-
-假如 /^　我打开网址<http:\/\/proj\.cdu\.edu\.cn>$/ do
-  pending # express the regexp above with the code you wish you had
+假设 /^　我打开网址"(.*?)"$/ do |arg1|
 end
 
-那么 /^　我应该看到<请输入用户名和密码进行登录>$/ do
-  pending # express the regexp above with the code you wish you had
+那么 /^　我应该看到"(.*?)"$/ do |arg1|
 end
 
-当 /^　我以(\d+)登录$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+当 /^　  我以"(.*?)"登录$/ do |arg1|
 end
 
-当 /^　填写登录密码(\d+)$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-那么 /^　我应该看到人事部$/ do
-  pending # express the regexp above with the code you wish you had
+当 /^　填写登录密码"(.*?)"$/ do |arg1|
 end
