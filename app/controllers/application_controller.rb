5a6d4e73be503bb53af3c7d1342cc57d3a149afc
simlegate <simlegate@163.com>
2013-01-28 01:17:23 +0800

    添加sso

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
- #before_filter CASClient::Frameworks::Rails::Filter
+  before_filter CASClient::Frameworks::Rails::Filter
 end
