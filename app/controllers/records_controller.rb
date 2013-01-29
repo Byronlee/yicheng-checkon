9774c6f1283618c40232fc6043f9b7794f032357
simlegate <simlegate@163.com>
2013-01-30 00:40:57 +0800

    merge

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -11,19 +11,11 @@ class RecordsController < ApplicationController
   end
 
   def create
-<<<<<<< HEAD
-   sta = params[:staff]
-   render :json => sta
-=======
-
     params[:record].each do | key , value |
        p key
        p value
     end
 
-
-
->>>>>>> b05df39a81f56baf90ee83b0c4f2d4f1e77e7d91
   end
 
   def update
