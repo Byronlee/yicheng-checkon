7f8b42c442c8f17f435507d669a4f1a8e819b3cc
Byronlee <lbyronlee@gmail.com>
2013-02-24 21:01:52 +0000

    为index的action添加功能

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,24 +1,26 @@
 class RecordsController < ApplicationController
 
   def index
-    @current_user
+    @ary = [] 
+    data = Record.state("init")
+    data.map do |v|
+      groupid = Webservice.getData("user/id/"+v.staffid)["SU_DEPT_ID"]
+      @ary << {staffid: v.staffid,groupid: groupid,created_at: v.created_at,group_name: Webservice.getData("dept/id/"+groupid)["SD_DEPT_NAME"]}
+    end
+    p "============="
+    p @ary
   end
 
   def new
     @resources = Webservice.dpt_users "dept/users/4028809b3c6fbaa7013c6fbc39900380"
   end
 
-
-
-
-
   def create
     params[:record].each do | key , value |
-       p key
-       p value
+      Record.attend key,value
     end
+    redirect_to :action => "index"
   end
-
 
 
   def update
