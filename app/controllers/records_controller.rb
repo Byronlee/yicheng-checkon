da654218ecdd486339a1812575415f4b38756bf0
Byronlee <lbyronlee@gmail.com>
2013-03-01 21:54:47 +0000

    批量考勤

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -6,7 +6,7 @@ class RecordsController < ApplicationController
 
  # 根据 考勤日期和 部门，返回 那些部门的考勤没有注册
   def index
-      if @records
+      if @records.any?
         @tasks= (@records.map{ |record| { :dept_id => User.new(record.staffid).dept_id, :attend_date => record.attend_date.to_s} }
                 ).uniq.map do |task|
                    {dept_id: task[:dept_id], attend_date: task[:attend_date] ,dept_name: Department.new(task[:dept_id]).name}
@@ -17,6 +17,7 @@ class RecordsController < ApplicationController
 
   def new
      @dept_id = params[:dept_id]
+     @dept_name = params[:dept_name]
      @time    = params[:time]
      @users = Department.new(@dept_id).users
   end
@@ -24,7 +25,7 @@ class RecordsController < ApplicationController
   def create
     
     # key 表示user_id value 是一个hash，他的key表示check_unit,value表示behave
-    @checkins ={}
+    @checkins =[]
     params[:record].each do | user_id , checks|
       record =  Record.find_by(staffid: user_id, attend_date: params[:time])
       checks.map do |unit_id,behave_id|
@@ -33,18 +34,28 @@ class RecordsController < ApplicationController
       record.checkins.update_all @chenckins
       record.register
     end
+    redirect_to :action => "index"
   end
 
-  
-    # redirect_to :action => "index"
+  def fast_register
+    p "444444444444444444444444"
+    p params
+  end
+
+  def show
+  end
+
 
   def whether_checkin 
     if params[:option]=="yes"
-      
+      current_user.attend_depts["children"].map do | dept | 
+         Department.new(dept["id"]).users.map do | user |
+          Record.create(staffid: user.id, attend_date: Time.now.to_date ,record_person: current_user.username, record_zone: dept["name"])
+        end
+      end
     end
+    redirect_to :action => "index"
   end
-
- 
 
   private 
    def initialize_records
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 class User
 
-  attr_accessor :nickname_code , :nickname_display , :phone_num , :username ,  :user_no , :dept_id , :id
+  attr_accessor :nickname_code , :nickname_display , :phone_num , :username ,  :user_no , :dept_id , :id , :dept_name
 
   def  initialize id 
     resource= Webservice.get_data "/user/id/"+id
@@ -11,6 +11,7 @@ class User
     @username= resource["SU_USERNAME"]
     @user_no= resource["SU_USER_NO"]
     @dept_id= resource["SU_DEPT_ID"]
+    @dept_name= Department.new(resource["SU_DEPT_ID"]).name
     @id = id
   end
 
@@ -18,4 +19,5 @@ class User
  def attend_depts
    Webservice.get_data("/attend/tree/"+@id)
  end
+
 end
