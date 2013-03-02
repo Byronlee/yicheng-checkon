4e119de38cf55f086fb498322d2eb2e3c40fac4f
Byronlee <lbyronlee@gmail.com>
2013-03-02 17:31:32 +0000

    add

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 class Record
   include Mongoid::Document
 
@@ -32,7 +31,6 @@ class Record
     end
   end
 
-<<<<<<< HEAD
  # validates_presence_of :period, :user
 #  validates_uniqueness_of :period, scope: [:user]
 
@@ -43,20 +41,18 @@ class Record
         record.checkins << Checkin.create!( record: record, check_unit: unit, behave: Behave.default )
       end
     end
-=======
+  end
   def self.whenever_add
      create!
->>>>>>> 7227f62d3bf38ae6a959fc14299ed0e458c4e557
   end
 
-def self.attend id,opt,time
-  record = Record.where(:created_at.gte => time,:created_at.lt => (time.to_time)+1.days,staffid: id).update(attend_option: opt,state: "saved")
-end
+  def self.attend id,opt,time
+    record = Record.where(:created_at.gte => time,:created_at.lt => (time.to_time)+1.days,staffid: id).update(attend_option: opt,state: "saved")
+  end
 
 
 
-def self.state state
-  where(state: state)
-end
-
+  def self.state state
+    where(state: state)
+  end
 end
