014b44146404a7acc0fbca532983c79b41a7fc05
Byronlee <lbyronlee@gmail.com>
2013-03-03 14:32:44 +0000

    修改页面显示

--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -1,7 +1,22 @@
+# -*- coding: utf-8 -*-
 module RecordsHelper
 
  def current_user
    @current_user
  end
 
+
+
+ def FormatDate time
+   case (Time.now.to_date - time.to_date)
+   when 0
+     "今天"
+   when 1
+     "昨天"
+   when 2
+     "前天"
+   else
+     time
+   end
+ end
 end
