182500dac716eb2d85391e459e31b5993ee52090
Byronlee <lbyronlee@gmail.com>
2013-03-11 14:47:02 +0000

    去掉cas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter CASClient::Frameworks::Rails::Filter
+# before_filter CASClient::Frameworks::Rails::Filter
   before_filter :current_user
 
   def current_user
