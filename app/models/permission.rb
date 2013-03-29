f5b05b3cecdcc9a300c31a33ae0dfbd1e4b20ed2
simlegate <simlegate@163.com>
2013-03-29 12:02:29 +0000

    添加role字符串

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -2,7 +2,7 @@ class Permission
 
   def self.assign roles
     roles.each do |role|
-      User.send :include ,Object.const_get(role)
+      User.send :include ,Object.const_get(role+"Role")
     end
   end
 end
