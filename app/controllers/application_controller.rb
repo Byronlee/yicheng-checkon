86094367326bfbd7ad392c59831137b57e818049
simlegate <simlegate@163.com>
2013-04-01 20:10:06 +0800

    备份

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter CASClient::Frameworks::Rails::Filter
   before_filter :current_user
-  load_and_authorize_resource
+# load_and_authorize_resource
 
   def current_user
     attrs = session[:cas_extra_attributes]["attrs"]
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,18 +2,18 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user = User.current_user
-
-    if user.role.include? "Registrar"
-      can :manage , Trainee
-      can :manage , Count
-      can :lanuch , WorkFlow
-      can :manage , StaffRecord
-    end
-    if user.role.include? "Approval" 
-      can :index , Task
-      cannot :manage , Trainee
-      cannot :manage , StaffRecord
-    end
+#    user = User.current_user
+#
+#    if user.role.include? "Registrar"
+#      can :manage , Trainee
+#      can :manage , Count
+#      can :lanuch , WorkFlow
+#      can :manage , StaffRecord
+#    end
+#    if user.role.include? "Approval" 
+#      can :index , Task
+#      cannot :manage , Trainee
+#      cannot :manage , StaffRecord
+#    end
   end
 end
