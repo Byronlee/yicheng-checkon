1ed7e1085aa2725eb5d6c30b72cfb706a0496fa4
simlegate <simlegate@163.com>
2013-04-14 14:40:49 +0800

    除去魔术数

--- app/models/approval_role.rb
+++ app/models/approval_role.rb
@@ -6,6 +6,6 @@ class ApprovalRole
   end
 
   def attend_depts   #只读“成都伊城”的部门结构
-    Webservice.get_data("dept_tree/4028809b3c6fbaa7013c6fbc39510002")
+    Webservice.get_data('dept_tree/' << Settings.approval_dept_id)
   end
 end
