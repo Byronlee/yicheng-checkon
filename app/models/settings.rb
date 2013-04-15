ba7e965f34abe13ea61e6862fffbe4f7fe635089
simlegate <simlegate@163.com>
2013-04-15 11:15:49 +0800

    让User实例化统一

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,4 +1,4 @@
 class Settings < Settingslogic
-  source "#{Rails.root}/config/application.yml"
+  source "#{Rails.root}/config/settings.yml"
   namespace Rails.env
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,38 +12,34 @@ class User
   field :dept_name
   field :dept_ancestors ,type:Array
   field :position , type:Array
-  # TODO ws should return roles
-  field :role , type:Array
 
-  after_initialize do |user|
-    if user.role
-      role.each do |r|
-        user.class_eval{ include Object.const_get(r + 'Role')}
-      end
+  attr_accessor :roles
+
+  def perssion roles_array
+    roles_array.each do |r|
+      class_eval{ include Object.const_get(r + 'Role')}
     end
+    # roles = roles_array why?
+    self.roles = roles_array
+    self
   end
 
-  def self.resource sid
-    if sid.instance_of?(String)
-      init_attr Webservice.get_data("/user/id/"+sid),sid
-    else
-      init_attr sid
-    end
+  def self.resource user_id
+    init_attr Webservice.get_data("/user/id/"+user_id)
   end
 
-  def self.init_attr rs,sid=nil
+  def self.init_attr rs
     attrs = {
-      staffid: sid || rs["SU_USER_ID"],
+      staffid:  rs["SU_USER_ID"],
       nickname_code: rs["SU_NICKNAME_CODE"],
       nickname_display: rs["SU_NICKNAME_DISPLAY"],
       phone_num: rs["SU_PHONE_NUM"],
       username: rs["SU_USERNAME"],
-      user_no: rs["SU_USER_NO"],
+      user_no: '0' + rs["SU_USER_NO"],
       dept_id: rs["SU_DEPT_ID"],
       dept_name: Department.new(rs["SU_DEPT_ID"]).name ,
       dept_ancestors: rs["DEPT_ANCESTORS"],
       position: rs["POSTS"],
-      role: rs["role"]
     }
     new(attrs)
   end
@@ -54,11 +50,11 @@ class User
   end
 
   def registrar?
-    role.include? "Registrar"
+    roles.include? "Registrar"
   end
 
   def approval?
-    role.include? "Approval"
+    roles.include? "Approval"
   end
 
   def depts_node
