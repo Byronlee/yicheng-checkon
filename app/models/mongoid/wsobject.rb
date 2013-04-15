6fd4306c1d47b9561861110c037a25216b897afc
simlegate <simlegate@163.com>
2013-04-15 14:26:15 +0800

    merge

--- app/models/mongoid/wsobject.rb
+++ app/models/mongoid/wsobject.rb
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
 module WsObject
     
-  def self.create_obj(hash)
-    hash.each do |key,value|
-     field_name  = Setings.#{k}
-        next if field_name
-             if field_name.eql("")
-
-       add readonly_attr with value = v
-    end
-  end
 end
 
