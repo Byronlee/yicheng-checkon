aae42b693938d393ebce983ecb466424ab889435
simlegate <simlegate@163.com>
2013-04-16 14:41:44 +0800

    重构任务首页

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -4,4 +4,8 @@ module TasksHelper
   def count_task tasks
     tasks.select{|s|s[:state]=='checking'}.count()
   end
+
+  def render_no_content
+    render "common/no_content" , :message =>  t('.no_content.no_task_tip')
+  end
 end
--- app/models/crontask.rb
+++ app/models/crontask.rb
@@ -21,7 +21,7 @@ class Crontask
             record_zone_name:   cu.dept_name 
            }
         end
-        StaffRecord.create!(users_hash)
+        StaffRecord.collection.insert(users_hash)
       end
 
       Trainee.scoped.map do | user |  
