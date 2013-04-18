1e1fd75d5cfb84d43f3015857223081fccfcffc0
simlegate <simlegate@163.com>
2013-04-18 10:14:12 +0800

    merge

--- app/helpers/counts_helper.rb
+++ app/helpers/counts_helper.rb
@@ -5,9 +5,11 @@ module CountsHelper
     ["所在位置","工号","姓名"] + type.all.map {|bh| bh.name}
   end
 
-
   def format v
     v.eql?(0) ? "" : v
   end
 
+  def behave_types
+    BehaveType.all.map{ |type| [type.name,type.id]}
+  end
 end
