84b99d62c1b8f3f1e0abb9f6479f6b35c7cc7d21
Byronlee <lbyronlee@gmail.com>
2013-04-25 17:45:07 +0000

    closed #58 完善异常页面提示，增加logo小图标

--- app/controllers/exceptions_controller.rb
+++ app/controllers/exceptions_controller.rb
@@ -2,9 +2,23 @@
 class ExceptionsController < ActionController::Base
 
 
-  def browser
+  def browser_error
    render layout: false
   end
+
+  def render_404
+    flash[:error]= "你所访问的资源不存在！"
+    render :error , :layout => false
+  end
   
+  def routing_error
+    flash[:error]= "你所访问的url不存在！"
+    render :error ,:layout => false
+  end
+
+  def cancan_error
+    flash[:error]= "你无权限访问此页面！"
+    render :error ,:layout => false
+  end
 
 end
