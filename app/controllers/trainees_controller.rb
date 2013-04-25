6f5c9b8b25b1b26ef49196fdd3977d37a6b5c46e
simlegate <simlegate@163.com>
2013-04-25 22:14:38 +0800

    #59 合并时需要双方都已经完成考勤任务，当双方的考勤数据重复时，无工号覆盖有工号的

--- app/controllers/trainees_controller.rb
+++ app/controllers/trainees_controller.rb
@@ -17,13 +17,16 @@ class TraineesController < ApplicationController
   end
 
   def merge
-    # params[:condition][:dept_id] stand for 'staff_id'
-    if (new_id = params[:condition][:dept_id]).blank?
+    # 当给无工号员工分配工号的
+    new_id = params[:dept][:user_id]
+    if (new_id = new_id).blank?
       flash[:error] = '请选择你需要合并的员工'
     elsif Trainee.find(params[:old_trainee_id]).finish?
       flash[:error] = '你还没有完成该无工号员工的考勤任务,请完成后重试!'
+    elsif StaffRecord.by_staffid_and_date(new_id,Date.today).first.checking?
+      flash[:error] = '该员工今天的考勤任务还没有完成，请先完成考勤任务后在合并!'
     else
-      @result = TraineeRecord.merge(params[:old_trainee_id],params[:condition][:dept_id])
+      @result = TraineeRecord.merge(params[:old_trainee_id],new_id)
       flash[:success] = '成功合并员工考勤数据'
     end
       redirect_to trainees_path
