1a8ff7e51456bb5f18155467ba1c88d9b1f9e2b9
simlegate <simlegate@163.com>
2013-04-26 12:18:34 +0800

    加上cas log

--- config/environment.rb
+++ config/environment.rb
@@ -4,16 +4,15 @@ require File.expand_path('../application', __FILE__)
 require 'casclient'
 require 'casclient/frameworks/rails/filter'
 
-#cas_logger = CASClient::Logger.new('/home/simlegate/workspace/yicheng-checkon/log/cas.log')
-#cas_logger.level = Logger::DEBUG
-
 Attendance::Application.initialize!
 
+cas_logger = CASClient::Logger.new("#{Rails.root}/log/cas.log")
+cas_logger.level = Logger::DEBUG
 CASClient::Frameworks::Rails::Filter.configure(
   :cas_base_url => Settings.cas_url,
   :username_session_key => :login,
   :extra_attributes_session_key => :cas_extra_attributes,
   :enable_single_sign_out => true,
-# :logger => cas_logger,
+  :logger => cas_logger,
   :authenticate_on_every_request => false
 )
