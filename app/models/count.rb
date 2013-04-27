241d1d9eb8d4afc1c5535adcdaa60607e2f76f0f
simlegate <simlegate@163.com>
2013-04-27 18:43:57 +0800

    去掉统计生成的临时表

--- app/models/count.rb
+++ app/models/count.rb
@@ -2,7 +2,7 @@
 class Count
   include Mongoid::Document
   include Mongoid::Count
- 
+
   class << self
 
     def select_records start,over,user_ids
@@ -11,25 +11,21 @@ class Count
       @records.in("checkins.behave_id" => default_count_behave_types ) 
     end
 
+    def excute_counts records
+      records.map_reduce(map,reduce).finalize(finalize).out(inline: 1).send(:documents)
+    end
 
-   def excute_counts records
-     Count.new(records.map_reduce(map,reduce).out(inline: 1).finalize(finalize).raw)
-   end
-
-
-  
-
-   def package_counts counts, result={},tmp = {}
-     Settings.count_types.map do |type,behaves|
+    def package_counts counts, result={},tmp = {}
+      Settings.count_types.map do |type,behaves|
         behaves.map do |behave,name|
-         behave_id = Behave.find_by(name: name).id
-         tmp[behave] = counts.where("_id" =>  1)
-       end
-       result[type] = tmp
-       tmp = {}
-     end
+          behave_id = Behave.find_by(name: name).id
+          tmp[behave] = counts.select{|x|x['_id']['behave_id'].eql?(behave_id)}
+        end
+        result[type] = tmp
+        tmp = {}
+      end
       result
-   end
+    end
   end  # class << self
 
   def behave_name
