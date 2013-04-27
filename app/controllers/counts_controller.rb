4be2f08c66ab862af53b594ac8cb42888d309a21
simlegate <simlegate@163.com>
2013-04-27 16:13:35 +0800

    merge

--- app/controllers/counts_controller.rb
+++ app/controllers/counts_controller.rb
@@ -2,7 +2,7 @@
 class CountsController < ApplicationController
 
   def index
-   @counts = Count.counts(current_user.dept_users_with_subdept)
+   @counts = Count.counts(current_user)
   end
 
   def create 
