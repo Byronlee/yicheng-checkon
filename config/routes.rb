24a31783173d81bbf16491eb8f3c624b997b6ba4
simlegate <simlegate@163.com>
2013-05-01 22:43:36 +0800

    增加counts路由测试,删除重复的路由

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Attendance::Application.routes.draw do
 
-  resources :counts do
-    collection do
-      get :amount
-    end
+  resources :counts , :only => [:index, :create] do
+    get :export ,:on => :collection
   end
 
   resources :trainees do
@@ -29,8 +27,6 @@ Attendance::Application.routes.draw do
   resources :perssions
 
   resources :notices
-  resources :counts
-  post 'counts/update'
 
   match 'logout'     => 'application#logout'
   match 'registrar'  => 'tasks#registrar' ,:as => :registrar
@@ -38,7 +34,6 @@ Attendance::Application.routes.draw do
   match 'ajax_attend_tree' => 'homes#ajax_attend_tree' ,:via => :post
   match 'ajax_dept_users' => 'homes#ajax_dept_users'
   match 'autocomplete/search_users' => 'homes#search_users'
-  match 'export' => 'counts#export'
   match 'proces_detail' =>  'examines#proces_datail' ,:as => :proces_detail ,:via => :post
 
   match 'cancan_error'    => 'exceptions#cancan_error'
--- spec/routing/task_routes_spec.rb
+++ spec/routing/task_routes_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 describe "routing to tasks" do
-  it "routes /tasks/registrar to tasks#registrar" do
+  it "routes /registrar to tasks#registrar" do
     expect(:get => "/registrar").to route_to(
       :controller => "tasks",
       :action => "registrar",
     )
   end
 
-  it "routes /tasks/approval to tasks#approval" do
+  it "routes /approval to tasks#approval" do
     expect(:get => "/approval").to route_to(
       :controller => "tasks",
       :action => "approval",
