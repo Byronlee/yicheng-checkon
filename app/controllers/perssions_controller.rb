d31d594df17614237bd9ff034cfd1228bc0eec9e
simlegate <simlegate@163.com>
2013-05-07 16:38:26 +0800

    添加权限配置页面

--- app/controllers/perssions_controller.rb
+++ app/controllers/perssions_controller.rb
@@ -1,5 +1,8 @@
 class PerssionsController < ApplicationController
 
   def index
+    dept_scope = $ACCESSOR.registrar_attend_scope(current_user.staffid)
+    tr_ids = $ACCESSOR.users_with_role(:tempregistrar,dept_scope) 
+    @users = tr_ids.map {|tr_id| User.resource(tr_id)}
   end
 end
