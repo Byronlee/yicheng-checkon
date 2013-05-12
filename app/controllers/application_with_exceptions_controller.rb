32cbd03c1ed5f78b8723121e05b60537cba03076
Byronlee <lbyronlee@gmail.com>
2013-05-12 18:43:47 +0000

    完成浏览器兼容范围的扩大

--- app/controllers/application_with_exceptions_controller.rb
+++ app/controllers/application_with_exceptions_controller.rb
@@ -17,6 +17,7 @@ class  ApplicationWithExceptionsController < ActionController::Base
   end
   
   def browser_error
+    @user_agent = UserAgentParser.parse(request.user_agent)
     render layout: false
   end
   
@@ -26,52 +27,12 @@ class  ApplicationWithExceptionsController < ActionController::Base
   end
   
   def browser_filter
-    user_agent =  request.user_agent
-    return if user_agent.nil?
-    user_agent = UserAgentParser.parse(user_agent)
-    p '_________________'
-    p user_agent
-    p user_agent.name
-    p user_agent.version.major
-    #if user_agent.name.downcase.eql?('ie') && user_agent.version.major.to_i < 10
-    debugger
- #   user_agent = user_agent.downcase 
- #   @users_browser ||= begin
- #                        if user_agent.index('msie') && !user_agent.index('opera') && !user_agent.index('webtv')
- #                          'ie'+user_agent[user_agent.index('msie')+5].chr
- #                        elsif user_agent.index('gecko/')
- #                          'gecko'
- #                        elsif user_agent.index('opera')
- #                          'opera'
- #                        elsif user_agent.index('konqueror')
- #                          'konqueror'
- #                        elsif user_agent.index('ipod')
- #                          'ipod'
- #                        elsif user_agent.index('ipad')
- #                          'ipad'
- #                        elsif user_agent.index('iphone')
- #                          'iphone'
- #                        elsif user_agent.index('chrome/')
- #                          'chrome'
- #                        elsif user_agent.index('applewebkit/')
- #                          'safari'
- #                        elsif user_agent.index('googlebot/')
- #                          'googlebot'
- #                        elsif user_agent.index('msnbot')
- #                          'msnbot'
- #                        elsif user_agent.index('yahoo! slurp')
- #                          'yahoobot'
- #                        elsif user_agent.index('mozilla/')
- #                          'gecko'
- #                        else
- #                          'unknown'
- #                        end
- #                      end
- #   # ['chrome', 'gecko', 'safari']  只能chrome 浏览器使用！
- #   unless ['chrome'].include?(@users_browser)  then
- #     redirect_to browser_path
- #   end
+    @user_agent = UserAgentParser.parse(request.user_agent)
+    return if @user_agent.nil? 
+    name = @user_agent.name.downcase
+    return redirect_to browser_path if name.eql?('ie') && @user_agent.version.major.to_i < 10
+    unless  ['chrome','safari','firefox','opera','sougo','maxthon'].include?(name)  then
+      return redirect_to browser_path
+    end
   end
-
-
 end
